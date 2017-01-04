2fe32e728143e4b7c9f509f74b40e19df1350ee0
sliceofcake <sliceofcake@users.noreply.github.com>
2017-01-03 19:06:20 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# TwitterAssist
-Download ":orig"-version twitter images with the press of a key.
+# TwitterAssist  
+Download ":orig"-version twitter images with the press of a key.  
+  
+Run this JavaScript in your browser when viewing a twitter page to add the ability to do the following process  
+(1) mouse your cursor over an image  
+(2) click the user-specified key on your keyboard  
+(3) the image has been downloaded to your default downloads folder, at the ":orig" quality, as specified by twitter  
+  
+This code also works to download other images, but no special actions will take place. In other words, you'll probably be downloading a poor-quality image because twitter initially only shows you lower-quality versions of the original.  
+  
+Use case  
+Downloading higher-quality images on twitter is difficult and time-consuming. Pretty much every image that you see on twitter is a downgraded version, to better fit its display size - if an image is tiny on-screen, then they use a tiny version of the image - you shouldn't be able to tell the difference. --but when I go to download images, I have to jump through some hoops to get to the original versions. Twitter is willing to show you all the way to ":large"-version images, which you can just right-click download. This is bad, but not that bad. If the tweet has multiple images though, you'll need to use your browser's HTML inspector, do some clicking, and open the link you found in a new page to download it. Beyond that, every higher-quality twitter image has "-large" or "-orig" appended it the end of its name, so you have to rename each file you download. This is difficult. Instead, this script lets you just hover over an image, hit a keyboard key, and it does all that messy work for you. It takes the link and makes it into an ":orig" version, then downloads it and cuts off the "-orig" from the filename.  
+  
+Everything that could go wrong, within reason  
+• twitter could change the way they set up link names, breaking this script  
---
+++ twitterAssist.js
@@ -0,0 +1,96 @@
+(function(){
+	var p = {
+		xN : null,
+		yN : null,
+		keyS : "1", // default to the alpha-1 key
+	};
+	document.addEventListener("mousemove",(function(p){return function(event){
+		p.xN = event.clientX;
+		p.yN = event.clientY;
+	};})(p));
+	document.addEventListener("keyup",(function(p){return function(event){
+		if (event.key !== p.keyS){return;}
+		
+		if (event.metaKey || event.altKey || event.altKey || event.ctrlKey){return;}
+		
+		if (p.xN === null || p.yN === null){return;}
+		
+		var el = document.elementFromPoint(p.xN,p.yN);
+		if (el === null){return;}
+		
+		var src = el.getAttribute("src");
+		if (src === null || src === ""){return;}
+		
+		var linkS = null;
+		var filenameS = null;
+		
+		// attempt to match special twitter images that need :orig appended
+		if (linkS === null || filenameS === null){
+			src.replace(/^(https?:\/\/pbs\.twimg\.com\/media\/([a-zA-Z0-9\-_]+\.[a-zA-Z0-9]+)):[a-zA-Z]+$/,function(match,p1,p2,offset,string){
+				linkS = p1+":orig";
+				filenameS = p2;
+			});}
+		
+		// fallback to regular src without fancy :orig rules
+		if (linkS === null || filenameS === null){
+			src.replace(/^(https?:\/\/.+?([^\/]+))$/,function(match,p1,p2,offset,string){
+				linkS = p1;
+				filenameS = p2;
+			});}
+		
+		// no clue what you just clicked, but I don't like it!
+		if (linkS === null || filenameS === null){return;}
+		
+		var downloadLink = document.createElement("a");
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET",linkS,true);
+		xhr.responseType = "blob";
+		xhr.onload = (function(downloadLink,filenameS){return function(){
+			var file = new Blob([this.response],{type:"application/octet-stream"});
+			downloadLink.download = filenameS;
+			downloadLink.href = window.URL.createObjectURL(file);
+			downloadLink.onclick = function(event){document.body.removeChild(event.target);};
+			downloadLink.style.display = "none";
+			document.body.appendChild(downloadLink);
+			downloadLink.click();
+		};})(downloadLink,filenameS);
+		xhr.send();
+	};})(p));
+	
+	//----
+	
+	var elMain = document.createElement("div");
+	elMain.style.position = "fixed";
+	elMain.style.bottom = "0px";
+	elMain.style.left = "0px";
+	elMain.style.width = "100%";
+	elMain.style.color = "hsla(0,0%,100%,1)";
+	elMain.style.backgroundColor = "hsla(0,0%,0%,0.8)";
+	elMain.style.padding = "4px 8px";
+	elMain.style.zIndex = "999999999";
+	
+	var elInput = document.createElement("input");
+	elInput.style.width = "70px";
+	elInput.style.color = "hsla(0,0%,100%,1)";
+	elInput.style.backgroundColor = "hsla(0,0%,0%,1)";
+	elInput.style.textAlign = "center";
+	elInput.value = p.keyS;
+	elInput.addEventListener("keydown",(function(p){return function(event){
+		this.value = p.keyS = event.key;
+		event.preventDefault();
+	};})(p));
+	
+	var elVersion = document.createElement("div");
+	elVersion.style.position = "absolute";
+	elVersion.style.bottom = "4px";
+	elVersion.style.right = "20px"; // padding of parent plus actual value
+	elVersion.style.color = "hsla(0,0%,100%,0.5)";
+	elVersion.style.textAlign = "right";
+	elVersion.textContent = "ver1";
+	
+	elMain.appendChild(elVersion);
+	elMain.appendChild(document.createTextNode("Hover over an image, then press your "));
+	elMain.appendChild(elInput);
+	elMain.appendChild(document.createTextNode(" key to download it. [<- click and type to edit keybinding]"));
+	document.body.appendChild(elMain);
+})();
